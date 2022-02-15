b2b480a79c59008e8c150f48814c1344d61397f7
Ankita <ankita_kumari@intuit.com>
2022-02-15 10:46:52 +0530

    first commit

---
+++ src/test/java/com/sdet/AppTest.java
@@ -0,0 +1,19 @@
+package com.sdet;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class AppTest {
+    @Test
+    public void testcase1(){
+        App myApp = new App();
+        Assert.assertEquals(0, myApp.userLogin("abc", "abc123"));
+    }
+
+    @Test
+    public void testcase2(){
+        App myApp = new App();
+        Assert.assertEquals(1, myApp.userLogin("abc","abc"));
+    }
+}
